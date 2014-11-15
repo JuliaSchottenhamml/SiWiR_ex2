27ad3a47e3222a2e9713860ce13691505c2690eb
XzzX <xzzx@xzzx.info>
2014-11-15 11:31:31 +0100

    basic implementation of red black guass seidel

--- rbgs.cpp
+++ rbgs.cpp
@@ -13,6 +13,9 @@ extern "C" {
 #endif
 
 #include	"Timer.h"
+#include	"grid.hpp"
+
+static const double k = 2*M_PI;
 
 /**
   Converts a string to an arbitrary type. >> operator must be defined for the target type.
@@ -26,6 +29,14 @@ T StringTo(const std::string& string){
 	std::stringstream stream(string);
 	stream >> valor;
 	return valor;
+}
+
+constexpr	double f(const double x, const double y){
+	return 4.0*M_PI*M_PI*sin(2.0*M_PI*x)*sinh(2.0*M_PI*y);
+}
+
+constexpr	double border(const double x, const double y){
+	return sin(2.0*M_PI*x)*sinh(2.0*M_PI*y);
 }
 
 int main(int argc, char **argv) {
@@ -64,6 +75,72 @@ int main(int argc, char **argv) {
 #endif
 
 	siwir::Timer	timer;
+	
+	double	hx = 2.0/nx;
+	double	hy = 1.0/ny;
+	
+	double	invHx2 = 1.0/hx/hx;
+	double	invHy2 = 1.0/hy/hy;
+	
+	double	preF = 1.0 / (2*invHx2 + 2*invHy2 + k*k);
+	
+	//init f
+	
+	Grid	fRed( (nx+2)/2, (ny+1) );
+	Grid	fBlack( (nx+2)/2, (ny+1) );
+
+	for (int y = 0; y < ny+1; ++y){
+		for (int x = (y&0x1); x < nx+2; x+=2){
+			fRed(x,y) = f( x*hx, y*hy );
+		}
+	}
+	
+	for (int y = 0; y < ny+1; ++y){
+		for (int x = ((y&0x1)^0x1); x < nx+2; x+=2){
+			fBlack(x,y) = f( x*hx, y*hy );
+		}
+	}
+	
+	//init u
+	
+	Grid	uRed( (nx+2)/2, (ny+1) );
+	Grid	uBlack( (nx+2)/2, (ny+1) );
+	
+	for (int y = 0; y < ny+1; ++y){
+		for (int x = (y&0x1); x < nx+2; x+=2){
+			if (y == ny){
+				uRed(x,y) = border( x*hx, y*hy);
+			} else {
+				uRed(x,y) = 0;
+			}
+		}
+	}
+	
+	for (int y = 0; y < ny+1; ++y){
+		for (int x = ((y&0x1)^0x1); x < nx+2; x+=2){
+			if (y == ny){
+				uBlack(x,y) = border( x*hx, y*hy);
+			} else {
+				uBlack(x,y) = 0;
+			}
+		}
+	}
+	
+	//calc u
+	
+	for (int runs = 0; runs<c; ++runs){
+		for (int y = 1; y < ny; ++y){
+			for (int x = ((y&0x1)^0x1)+1; x < nx; x+=2){
+				uRed(x,y) = preF * (fRed(x,y) + invHx2 * (uBlack(x-1,y) + uBlack(x+1,y)) + invHy2 * (uBlack(x, y-1) + uBlack(x,y+1)) );
+			}
+		}
+		
+		for (int y = 1; y < ny; ++y){
+			for (int x = (y&0x1)+1; x < nx; x+=2){
+				uBlack(x,y) = preF * (fBlack(x,y) + invHx2 * (uRed(x-1,y) + uRed(x+1,y)) + invHy2 * (uRed(x, y-1) + uRed(x,y+1)) );
+			}
+		}
+	}
 
 	time = timer.elapsed();
 	std::cout << "time," << time << std::endl;
@@ -76,5 +153,25 @@ int main(int argc, char **argv) {
 	///******************************************************
 	///********************** OUTPUT ************************
 	///******************************************************
-
-	};
+	
+	std::ofstream	fOut("data/solution.txt");
+	for (int y = 0; y < ny+1; ++y){
+		for (int x = 0; x < nx+1; ++x){
+			if (y%2 == 0){
+				if (x%2 == 0){
+					fOut << x*hx << "\t" << y*hy << "\t" << uRed(x,y) << std::endl;
+				} else {
+					fOut << x*hx << "\t" << y*hy << "\t" << uBlack(x,y) << std::endl;
+				}
+			} else {
+				if (x%2 == 1){
+					fOut << x*hx << "\t" << y*hy << "\t" << uRed(x,y) << std::endl;
+				} else {
+					fOut << x*hx << "\t" << y*hy << "\t" << uBlack(x,y) << std::endl;
+				}			
+			}
+		}
+		fOut << std::endl;
+	}
+	fOut.close();
+};
